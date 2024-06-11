fd8fb37295c60cef497a9007424a95139c39fb1d
Habi <habi.thapa@gmail.com>
2024-06-11 23:57:29 +0545

    Moleculer basics

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-import { ServiceBroker } from "moleculer";
-
-const broker = new ServiceBroker();
-
-//Greeter Service
-broker.createService({
-  name: "greeter",
-  actions: {
-    sayHello(ctx) {
-      return `Hello ${ctx.params.name}`;
-    },
-  },
-});
+import UserService from "./services/user.service.js";
+import EmailService from "./services/email.service.js";
+import AuthService from "./services/auth.service.js";
 
 async function startApp() {
-  await broker.start();
-  const res = await broker.call("greeter.sayHello", { name: "John" });
-  console.log(res);
-  broker.stop();
+  await UserService.start();
+  await EmailService.start();
+  await AuthService.start();
+
+  try {
+    //simulate user creation
+    const newUser = await UserService.call("user.createUser", {
+      username: "hari",
+      email: "john@gmail.com",
+    });
+    console.log("New User Created", newUser);
+    const users = await UserService.call("user.getUsers");
+    console.log("All Users", users);
+    //Simulate sending email
+    const emailResult = EmailService.call("email.sendEmail", {
+      recipient: newUser.email,
+      subject: "Welcome",
+      content: "Thank you for signing up",
+    });
+    console.log(emailResult);
+    //Simulate auth result
+    const authResult = await AuthService.call("auth.authUser", {
+      username: newUser.username,
+      password: "admin",
+    });
+    console.log(authResult);
+  } catch (error) {
+    console.log("Error", error);
+  } finally {
+    await UserService.stop();
+    await EmailService.stop();
+    await AuthService.stop();
+  }
 }
 
 startApp();
---
+++ services/auth.service.js
@@ -0,0 +1,18 @@
+import { ServiceBroker } from "moleculer";
+
+const broker = new ServiceBroker();
+
+broker.createService({
+  name: "auth",
+  actions: {
+    async authUser(ctx) {
+      const { username, password } = ctx.params;
+      if (username === "admin" && password === "admin") {
+        return { success: true, message: "Auth was success" };
+      }
+      return { success: false, message: "Auth failed" };
+    },
+  },
+});
+
+export default broker;
